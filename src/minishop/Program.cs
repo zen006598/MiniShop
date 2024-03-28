94fd1a0a47f867fbeb2b85bf46efb5f874ffd05b
Jeff Chen <zen00326598@gmail.com>
2024-03-28 19:43:04 +0800

    config:(log)add the serilog

--- src/minishop/Program.cs
+++ src/minishop/Program.cs
@@ -2,52 +2,74 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using minishop.Data;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//Db connection with user secrets
-var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"))
+//log config from appsettings.json
+Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Configuration)
+   .CreateLogger();
+try
 {
-    Password = builder.Configuration["DefaultConnection:Password"]
-} ?? throw new InvalidOperationException("Connection string 'minishopIdentityDbContextConnection' not found.");
-var mssqlConnection = conStrBuilder.ConnectionString;
+    //Db connection with user secrets
+    var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"))
+    {
+        Password = builder.Configuration["DefaultConnection:Password"]
+    } ?? throw new InvalidOperationException("Connection string 'minishopIdentityDbContextConnection' not found.");
+    var mssqlConnection = conStrBuilder.ConnectionString;
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(mssqlConnection));
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(mssqlConnection));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-   {
-       options.Password.RequireDigit = false;
-       options.Password.RequiredLength = 6;
-       options.Password.RequireNonAlphanumeric = false;
-       options.Password.RequireUppercase = false;
-       options.Password.RequireLowercase = true;
-   }
-).AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+       {
+           options.Password.RequireDigit = false;
+           options.Password.RequiredLength = 6;
+           options.Password.RequireNonAlphanumeric = false;
+           options.Password.RequireUppercase = false;
+           options.Password.RequireLowercase = true;
+       }
+    ).AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+    builder.Host.UseSerilog();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+    app.UseAuthentication(); ;
+
+    app.UseAuthorization();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.MapRazorPages();
+
+    app.Run();
+
+    return 0;
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication(); ;
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Error(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
