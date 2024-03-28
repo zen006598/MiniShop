ed0dc2301e01cbb52c77a3ed25c6144564ed90e9
Jeff Chen <zen00326598@gmail.com>
2024-03-28 19:31:55 +0800

    feat:(identity user) 新增default identity

--- src/minishop/Program.cs
+++ src/minishop/Program.cs
@@ -1,15 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using minishop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Db connection with user secrets
+var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"))
+{
+    Password = builder.Configuration["DefaultConnection:Password"]
+} ?? throw new InvalidOperationException("Connection string 'minishopIdentityDbContextConnection' not found.");
+var mssqlConnection = conStrBuilder.ConnectionString;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(mssqlConnection));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+   {
+       options.Password.RequireDigit = false;
+       options.Password.RequiredLength = 6;
+       options.Password.RequireNonAlphanumeric = false;
+       options.Password.RequireUppercase = false;
+       options.Password.RequireLowercase = true;
+   }
+).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,11 +40,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
